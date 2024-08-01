9b55ccd058bd7b05404ba7a03d54bce28ff6cb99
nextc <1391040917@qq.com>
2024-08-01 11:25:35 +0800

    feat: 完善 ref 能够传入复杂数据类型

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,35 +1,46 @@
 import { trackEffects, triggerEffects, isTracking } from './effect'
-import { hasChanged } from '../shared'
+import { reactive } from './reactive'
+import { isObject, hasChanged } from '../shared'
 
 class RefImpl {
   private _value: any
   public dep: any
+  private _rawValue: any
 
   constructor(value: any) {
-    this._value = value
+    this._rawValue = value
+    this._value = isObject(value) ? reactive(value) : value
     this.dep = new Set()
   }
 
   get value() {
-    trackRefValue(this)
+    this.trackRefValue()
     return this._value
   }
 
   set value(newValue) {
-    if (hasChanged(this._value, newValue)) {
-      this._value = newValue
+    if (hasChanged(this._rawValue, newValue)) {
+      this._rawValue = newValue
+      this._value = isObject(newValue) ? reactive(newValue) : newValue
       // 派发更新，把集合里的函数都再执行一次
       triggerEffects(this.dep)
     }
   }
-}
 
-const trackRefValue = (ref: RefImpl) => {
-  if (isTracking()) {
-    // 收集依赖，把副作用函数放到集合里
-    trackEffects(ref.dep)
+  trackRefValue() {
+    if (isTracking() && !isObject(this._rawValue)) {
+      // 收集依赖，把副作用函数放到集合里
+      trackEffects(this.dep)
+    }
   }
 }
+
+// const trackRefValue = (ref: RefImpl) => {
+//   if (isTracking() && !isObject(ref.value)) {
+//     // 收集依赖，把副作用函数放到集合里
+//     trackEffects(ref.dep)
+//   }
+// }
 
 export const ref = (value: any) => {
   return new RefImpl(value)
